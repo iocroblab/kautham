29504f27adea11221c9cf593673210785a66c894
Jan Rosell <jan.rosell@upc.edu>
2011-08-30 18:21:44 +0000

    - Added Matlab files to analyze guibroplanner results

---
+++ examples/BRONCO/Matlab/evalall.m
@@ -0,0 +1,72 @@
+%evalresults
+clear v
+%compare the use of random
+i=1;
+v(:,:,i)=r604;
+i=i+1;
+v(:,:,i)=r604r;
+i=i+1;
+v(:,:,i)=r604r2;
+i=i+1;
+v(:,:,i)=r604r3;
+evalpaths(v)
+return 
+
+%eval the use of alpha weight
+i=1;
+v(:,:,i)=r1000;
+i=i+1;
+v(:,:,i)=r802;
+i=i+1;
+v(:,:,i)=r604;
+evalpaths(v)
+return 
+%CONCLUSIONS:
+%Va be el 604 amb cost alpha 
+%rAlpha[i] = 0.5*(1-abs(beta[i])*abs(alpha[i])+0.5*abs(alpha[i]-alpha0));
+
+
+%compare the use of different alpha weights
+i=1;
+v(:,:,i)=r604;
+i=i+1;
+v(:,:,i)=r604a;
+i=i+1;
+v(:,:,i)=r604b;
+evalpaths(v)
+return 
+
+
+
+%compare the use of different alpha weights
+i=1;
+v(:,:,i)=r802;
+i=i+1;
+v(:,:,i)=r802a;
+evalpaths(v)
+return 
+
+
+%compare the use of different alpha weights
+i=1;
+v(:,:,i)=r802;
+i=i+1;
+v(:,:,i)=r604;
+i=i+1;
+v(:,:,i)=r622;
+evalpaths(v)
+return 
+%compare the use of different alpha weights
+i=1;
+v(:,:,i)=r1000;
+i=i+1;
+v(:,:,i)=r820;
+evalpaths(v)
+return 
+
+
+
+
+
+
+
