1fa00b6a6530bf2dbff2f504ca8ab9f691b92b4b
Alexander Pérez Ruiz <aperezr@gmail.com>
2012-03-19 12:14:12 +0000

    - The ioccom folder was deleted. - Minor changes in the data_ioc_cell.

--- libutil/data_ioc_cell.hpp
+++ libutil/data_ioc_cell.hpp
@@ -2,8 +2,18 @@
 #define _KAUTHAM_IPC_H
 
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
+//#include <boost/interprocess/managed_shared_memory.hpp>
+//#include <boost/interprocess/containers/vector.hpp>
+//#include <boost/interprocess/allocators/allocator.hpp>
 
 namespace kautham{
+  /////Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
+  /////This allocator will allow placing containers in the segment
+  //typedef boost::interprocess::allocator<int, boost::interprocess::managed_shared_memory::segment_manager>  ShmemAllo;
+
+  /////Alias a vector that uses the previous STL-like allocator so that allocates
+  /////its values from the segment
+  //typedef boost::interprocess::vector<int, ShmemAllo> PathVector;
   struct robot_data{
     robot_data(){
       for(int i=0; i < 6; i++){
@@ -18,15 +28,23 @@ namespace kautham{
   struct data_ioc_cell{
     data_ioc_cell(){
       synchronized = false;
+      //pathChange = false;
+      //path = segment.construct<PathVector>("DesPath")(ShmemAllo);
     }
     //Items to fill
-    bool       synchronized;
-    robot_data r1_state;
-    robot_data r1_desired;
-    robot_data r2_state;
-    robot_data r2_desired;
+    bool        synchronized;
+    robot_data  r1_state;
+    robot_data  r1_desired;
+    robot_data  r2_state;
+    robot_data  r2_desired;
     //Mutex to protect access to the data
     boost::interprocess::interprocess_mutex mutex_in, mutex_out;
+
+    //// Data about the path
+    //bool        pathChange;
+    //PathVector* path;
+    ////Mutex to protect access to the data
+    //boost::interprocess::interprocess_mutex mutpath_in, mutpath_out;
   };
 }
 
