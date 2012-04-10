1145491966c391dc607eeb155ae4f79680c20a9d
Alexander Pérez Ruiz <aperezr@gmail.com>
2012-04-10 15:55:24 +0000

    - Adding the Emulation mode to the remoteServer application. In this mode, two kautham modules can be interconnected without moving the real robots of the cell in order to test the simulation of the guiding with two teleoperators each one with a haptic device.

--- kauth_ROS/src/remoteserver.cpp
+++ kauth_ROS/src/remoteserver.cpp
@@ -21,31 +21,38 @@
 Connection    valClientR1;
 Connection    valClientR2;
 
-// Creating the two SOAP Cleints.
+// Creating the two SOAP Clients.
 CS8Robot      _robotSoap1;
 CS8Robot      _robotSoap2;
+
+// The emulation variables.
+vector<double> _emuRobot1;
+vector<double> _emuRobot2;
 
 std::string   STAUBLI_IP_R1;
 std::string   STAUBLI_IP_R2;
 std::string   SOAP_IP_R1;
 std::string   SOAP_IP_R2;
-enum connType{VAL, SOAP} _activeConn;
+enum connType{VAL, SOAP, EMU} _activeConn;
 
 std::string   PORT="";
 
 void robot1Callback(const sensor_msgs::JointState::ConstPtr& r1des)
 {
-  if( _activeConn == VAL ){
-  // Now I send this data to the CS8 using val program and simple udp socket.
-  std::string smsg;
-  for (unsigned int i=0; i<r1des->position.size(); i++){
-      smsg.append(boost::lexical_cast<std::string>(r1des->position[i]));
-      smsg.append(" ");
-  }
-  smsg.append("@");
+  switch( _activeConn ){
+  case VAL:
+	  // Now I send this data to the CS8 using val program and simple udp socket.
+	  std::string smsg;
+	  for (unsigned int i=0; i<r1des->position.size(); i++){
+		  smsg.append(boost::lexical_cast<std::string>(r1des->position[i]));
+		  smsg.append(" ");
+	  }
+	  smsg.append("@");
 
-  valClientR1.send(smsg);
-  }else{
+	  valClientR1.send(smsg);
+	  break;
+	  
+  case SOAP:
 	try{
 	  RobotJointPos joints;
 	  joints.resize(6);
@@ -59,6 +66,10 @@ void robot1Callback(const sensor_msgs::JointState::ConstPtr& r1des)
       _robotSoap1.setPower(false);
       
     }
+	break;
+  case EMU:
+    for (size_t i=0; i<r1des->position.size(); ++i)
+	  _emuRobot2.at(i) = r1des->position.at(i);
   }
 
 #ifdef _DEBUG
@@ -71,17 +82,20 @@ void robot1Callback(const sensor_msgs::JointState::ConstPtr& r1des)
 }
 
 void robot2Callback(const sensor_msgs::JointState::ConstPtr& r2des){
-if( _activeConn == VAL ){
-  // Now I send this data to the CS8 using val program and simple udp socket.
-  std::string smsg;
-  for (unsigned int i=0; i<r2des->position.size(); i++){
-      smsg.append(boost::lexical_cast<std::string>(r2des->position[i]));
-      smsg.append(" ");
-  }
-  smsg.append("@");
+switch( _activeConn ){
+  case VAL:
+	  // Now I send this data to the CS8 using val program and simple udp socket.
+	  std::string smsg;
+	  for (unsigned int i=0; i<r2des->position.size(); i++){
+		  smsg.append(boost::lexical_cast<std::string>(r2des->position[i]));
+		  smsg.append(" ");
+	  }
+	  smsg.append("@");
 
-  valClientR2.send(smsg);
-  }else{
+	  valClientR2.send(smsg);
+	  break;
+	  
+  case SOAP:
 	try{
 	  RobotJointPos joints;
 	  joints.resize(6);
@@ -95,9 +109,12 @@ if( _activeConn == VAL ){
       _robotSoap1.setPower(false);
       
     }
+	break;
+	
+  case EMU:
+    for (size_t i=0; i<r2des->position.size(); ++i)
+	  _emuRobot2.at(i) = r2des->position.at(i);
   }
-  
-
 
 #ifdef _DEBUG
 //  std::stringstream ss;
@@ -106,6 +123,16 @@ if( _activeConn == VAL ){
   ROS_INFO( "Received R2_Command");
 #endif
 }
+
+
+// This program have been designed to run in the Quasar machine in the IOC lab.
+// This program is part of the ROS nodes used by the Kautham project in order to 
+// take the capability to teleoperate the Staubli robots and to have haptic 
+// feedback of this teleoperation procedure.
+// The program could be connected to the CS8 controller using either VAL or 
+// SOAP protocols. Furthermore, the program can be used in EMU mode. This 
+// execution modality provides the emulation capability in order to connect 
+// two simulation nodes with the remote Cell avoiding the robot movements.
 
 int main(int argc, char **argv){
 
@@ -119,7 +146,7 @@ int main(int argc, char **argv){
 	else if ( _vargv[2] == SOAP )
 		_activeConn = SOAP;
 	else{
-		std::cout << "remoteServer VAL/SOAP ip_R1 ip_R2 freq.\nRemember it.\n";
+		std::cout << "remoteServer VAL/SOAP/EMU ip_R1 ip_R2 freq.\nRemember it.\n";
 		return -1;
 	}
 		
@@ -133,13 +160,19 @@ int main(int argc, char **argv){
     SOAP_IP_R2.append(":5653");
 
   }else{
-    std::cout << "remoteServer VAL/SOAP ip_R1 ip_R2 freq.\nRemember it.\n";
-    return -1;
+    if ( _vargv[2] == EMU ){
+	    _activeConn = EMU;
+		
+	}else{
+		std::cout << "remoteServer VAL/SOAP/EMU ip_R1 ip_R2 freq.\nRemember it.\n";
+		return -1;
+	}
   }
 
   // The connection only modifies the send data to the CS8 controller.
   // The data always are readed using SOAP.
-  if( _activeConn == VAL ){
+  switch( _activeConn ){
+  case VAL:
 	valClientR1.connect_to(STAUBLI_IP_R1,"5558");
 	valClientR2.connect_to(STAUBLI_IP_R2,"5558");
 	try{
@@ -152,7 +185,9 @@ int main(int argc, char **argv){
 	  _robotSoap1.setPower(false) ;
 	  _robotSoap2.setPower(false) ;
 	}
-  }else{ // Here use only the SOAP
+	break;
+	
+  case SOAP: // Here use only the SOAP
 	try{
 	  _robotSoap1.setURL(SOAP_IP_R1);
 	  _robotSoap1.login("default","");
@@ -167,12 +202,15 @@ int main(int argc, char **argv){
 	  _robotSoap1.setPower(false) ;
 	  _robotSoap2.setPower(false) ;
 	}
+	break;
+	
+  case EMU:
+	_emuRobot1.resize(6);
+	_emuRobot2.resize(6);
+		
   }
 	
-	
-
   std::cout << "We have connected to the two Stäubli TX" << std::endl;
-
   ros::init(argc, argv, "remoteServer");
 
   ros::NodeHandle n;
@@ -181,7 +219,7 @@ int main(int argc, char **argv){
   ros::Subscriber _robot1_subs = n.subscribe("R1_command", 1000, robot1Callback);
   ros::Subscriber _robot2_subs = n.subscribe("R2_command", 1000, robot2Callback);
 
-  ros::Rate loop_rate(atoi(argv[3]));
+  ros::Rate loop_rate(atoi(argv[5]));
 
   std::vector<double> target_pos(6, 0.);
 
@@ -191,8 +229,12 @@ int main(int argc, char **argv){
     _r1_state_message.header.stamp = ros::Time::now();
     _r1_state_message.position.resize(6);
 
-    _robotSoap1.GetRobotJoints(target_pos);
-    std::copy( target_pos.begin(),target_pos.end(), _r1_state_message.position.begin() );
+	if( _activeConn == EMU ){
+	  std::copy( _emuRobot1.begin(),_emuRobot1.end(), _r1_state_message.position.begin() );
+	}else{
+		_robotSoap1.GetRobotJoints(target_pos);
+		std::copy( target_pos.begin(),target_pos.end(), _r1_state_message.position.begin() );
+	}
 
 #ifdef _DEBUG
     _r1_state_message.name.resize(6);
@@ -208,9 +250,12 @@ int main(int argc, char **argv){
     _r2_state_message.header.stamp = ros::Time::now();
     _r2_state_message.position.resize(6);
 
-    _robotSoap2.GetRobotJoints(target_pos);
-
-    std::copy( target_pos.begin(),target_pos.end(), _r2_state_message.position.begin() );
+	if( _activeConn == EMU ){
+	  std::copy( _emuRobot2.begin(),_emuRobot2.end(), _r2_state_message.position.begin() );
+	}else{
+      _robotSoap2.GetRobotJoints(target_pos);
+      std::copy( target_pos.begin(),target_pos.end(), _r2_state_message.position.begin() );
+	}
 
 #ifdef _DEBUG
     _r2_state_message.name.resize(6);
