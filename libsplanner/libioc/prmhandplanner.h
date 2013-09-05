39d2069067fa363768582455520a8369a973a48e
Jan Rosell <jan.rosell@upc.edu>
2013-09-05 14:31:15 +0000

    moving lcprng.h to libsutil

--- libsplanner/libioc/prmhandplanner.h
+++ libsplanner/libioc/prmhandplanner.h
@@ -40,6 +40,7 @@
  ***************************************************************************/
  
 #include "prmplanner.h"
+#include <libsampling/lcprng.h>
 
 #if !defined(_PRMHANDPLANNER_H)
 #define _PRMHANDPLANNER_H
