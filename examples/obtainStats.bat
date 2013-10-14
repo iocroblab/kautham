aa8387230ea7047a832839af3a2b63fcd3c94a5d
Jan Rosell <jan.rosell@upc.edu>
2013-10-14 15:30:24 +0000

    DELETED EXAMPLES FOLDER

--- examples/obtainStats.bat
+++
@@ -1,5 +0,0 @@
-mpiexec -n 4 -log KauthamConsole.exe TX90_RHand_auro_can_table.xml 1000
-mpiexec -n 4 KauthamConsole.exe TX90_RHand_ICRA10_5PMD_CanBox.xml 1000
-mpiexec -n 4 KauthamConsole.exe TX90_RHand_ICRA10_5PMD_ShelfCan.xml 1000
-mpiexec -n 4 KauthamConsole.exe TX90_RHand_ICRA10_5PMD_ShelfT.xml 1000
-mpiexec -n 4 KauthamConsole.exe TX90_RHand_ICRA10_5PMD_TwoCanb.xml 1000
