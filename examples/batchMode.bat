5b8360758d7a56878a21908aa7ecf4f50ef7e9f9
Jan Rosell <jan.rosell@upc.edu>
2013-10-14 15:30:24 +0000

    DELETED EXAMPLES FOLDER

--- examples/batchMode.bat
+++
@@ -1,5 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%COPY THIS FILE INTO THE build\Debug folder
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-mpiexec -n 4 KauthamConsole.exe ..\..\examples\table_rooms_R2_DRM_easy.xml 3
