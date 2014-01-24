b4982d56d7adf54b60e874e143de0ebe7ab627a0
Jan Rosell <jan.rosell@upc.edu>
2014-01-24 13:24:49 +0000

    Some improvements in RRTstar planner Changet ompl:geometric planeers to use ValidityChecker class Correctes some bug in guibro widget

--- libsplanner/libompl/omplTRRTplanner.cpp
+++ libsplanner/libompl/omplTRRTplanner.cpp
@@ -47,6 +47,7 @@
 #include <boost/bind/mem_fn.hpp>
 
 #include "omplTRRTplanner.h"
+#include "omplValidityChecker.h"
 
 
 
@@ -68,7 +69,9 @@ namespace Kautham {
         ss = ((og::SimpleSetupPtr) new og::SimpleSetup(space));
         ob::SpaceInformationPtr si=ss->getSpaceInformation();
         //set validity checker
-        ss->setStateValidityChecker(boost::bind(&omplplanner::isStateValid, si.get(), _1, (Planner*)this));
+        si->setStateValidityChecker(ob::StateValidityCheckerPtr(new omplplanner::ValidityChecker(si,  (Planner*)this)));
+        //7ss->setStateValidityChecker(boost::bind(&omplplanner::isStateValid, si.get(), _1, (Planner*)this));
+
         //alloc valid state sampler
         si->setValidStateSamplerAllocator(boost::bind(&omplplanner::allocValidStateSampler, _1, (Planner*)this));
         //alloc state sampler
