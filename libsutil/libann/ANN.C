953877f5abe1fffb235625bb95c944bcf841c875
Nestor Garcia <nestorgh91@gmail.com>
2014-04-19 20:52:22 +0200

    Solved minor bugs

--- libsutil/libann/ANN.C
+++ libsutil/libann/ANN.C
@@ -197,7 +197,7 @@ ANNbool ANNorthRect::inside(int dim, ANNpoint p)
 //  Error handler
 //----------------------------------------------------------------------
 
-void annError(char *msg, ANNerr level)
+void annError(const char *msg, ANNerr level)
 {
     if (level == ANNabort) {
 	std::cerr << "ANN: ERROR------->" << msg << "<-------------ERROR\n";
