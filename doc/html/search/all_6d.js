61e8384dbfdef86c58a3f0496cad9a53ffc44774
Néstor García Hidalgo <nestorgh91@gmail.com>
2014-04-19 20:51:23 +0200

    Documentation for the new class State

--- doc/html/search/all_6d.js
+++ doc/html/search/all_6d.js
@@ -20,6 +20,7 @@ var searchData=
   ['movable',['movable',['../class_kautham_1_1_link.html#a5bb7a020f7e4db361fe7ac7b0eea9536',1,'Kautham::Link']]],
   ['movealongpath',['moveAlongPath',['../class_kautham_1_1_i_o_c_1_1_p_r_m_planner.html#a6bc99c990589f27e8054ca443ec072f6',1,'Kautham::IOC::PRMPlanner::moveAlongPath()'],['../class_kautham_1_1_i_o_c_1_1_p_r_m_robot_hand_const_planner_i_c_r_a.html#ad74fda412b3f874e003e3472b0054991',1,'Kautham::IOC::PRMRobotHandConstPlannerICRA::moveAlongPath()']]],
   ['moveattachedobj',['moveAttachedObj',['../class_kautham_1_1_robot.html#a09cb329a7b639a6dccb6bd9c760ed216',1,'Kautham::Robot']]],
+  ['moveobstaclesto',['moveObstaclesTo',['../class_kautham_1_1_work_space.html#a9e09981c9f785f7d2e7a08f65a6334ea',1,'Kautham::WorkSpace']]],
   ['moverobotsto',['moveRobotsTo',['../class_kautham_1_1_work_space.html#ae24b0ad65f07de295d54355fc3105f63',1,'Kautham::WorkSpace']]],
   ['mu',['mu',['../classode__contact__coefficients.html#aabdf2b7a0beff4f790cdba36c1c2f715',1,'ode_contact_coefficients::mu()'],['../classurdf__contact__coefficients.html#a852cf641a4ea00c0e35282aac8053b1c',1,'urdf_contact_coefficients::mu()']]],
   ['multiply',['multiply',['../class_kautham_1_1_t_mat.html#a287aa6ce8ae7f3a80ca37cfd63e2eef3',1,'Kautham::TMat::multiply(const int k)'],['../class_kautham_1_1_t_mat.html#a92a0ed9187a2adee7399dd74b82a8ce5',1,'Kautham::TMat::multiply(const char *const w, char *const res) const '],['../class_kautham_1_1_t_mat.html#ae5b2b1da69ec2292da0c000c41b8b267',1,'Kautham::TMat::multiply(const char *const *const v, const int m) const ']]],
@@ -30,9 +31,9 @@ var searchData=
   ['mygridplanner',['MyGridPlanner',['../class_kautham_1_1_i_o_c_1_1_my_grid_planner.html',1,'Kautham::IOC']]],
   ['myplanner',['MyPlanner',['../class_kautham_1_1_i_o_c_1_1_my_planner.html#a00472c9155ca1208a0ffc18d0d4a265a',1,'Kautham::IOC::MyPlanner']]],
   ['myplanner',['MyPlanner',['../class_kautham_1_1_i_o_c_1_1_my_planner.html',1,'Kautham::IOC']]],
-  ['myprm',['myPRM',['../class_kautham_1_1omplplanner_1_1my_p_r_m.html',1,'Kautham::omplplanner']]],
   ['myprm',['myPRM',['../class_kautham_1_1omplplanner_1_1my_p_r_m.html#af062fab4e5b43515fe7985f962c94e0b',1,'Kautham::omplplanner::myPRM']]],
-  ['myprmplanner',['MyPRMPlanner',['../class_kautham_1_1_i_o_c_1_1_my_p_r_m_planner.html',1,'Kautham::IOC']]],
+  ['myprm',['myPRM',['../class_kautham_1_1omplplanner_1_1my_p_r_m.html',1,'Kautham::omplplanner']]],
   ['myprmplanner',['MyPRMPlanner',['../class_kautham_1_1_i_o_c_1_1_my_p_r_m_planner.html#a8ac634cc78d416854760b67d4f1f818d',1,'Kautham::IOC::MyPRMPlanner']]],
+  ['myprmplanner',['MyPRMPlanner',['../class_kautham_1_1_i_o_c_1_1_my_p_r_m_planner.html',1,'Kautham::IOC']]],
   ['myrrtstar',['myRRTstar',['../class_kautham_1_1omplplanner_1_1my_r_r_tstar.html',1,'Kautham::omplplanner']]]
 ];
