93292364debc4003619bbef1ed48f00fd12f6cd6
Néstor García Hidalgo <nestorgh91@gmail.com>
2014-04-22 12:24:42 +0200

    New demos added

---
+++ demos/IOC_demos/HandArmPRMPCA_AURO2013/experiments/batchModePCA.bat
@@ -0,0 +1,70 @@
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_1-1-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-1-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_5-5-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-1-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-5-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_10-10-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-1-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-5-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-10-4-20.xml 10
+
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-3-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-3-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-3-20.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-4-4.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-4-10.xml 10
+mpiexec -n 2 KauthamConsole.exe ..\..\examples\HandArmPRMPCA\TX90_RHand_PCA_Extreme_T_PC_20-20-4-20.xml 10
+
