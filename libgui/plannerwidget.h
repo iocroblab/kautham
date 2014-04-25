fac9d8339867b72ac1497654f09b1122e8b6b701
Néstor García Hidalgo <nestorgh91@gmail.com>
2014-04-25 10:39:30 +0200

    Updated Authors

--- libgui/plannerwidget.h
+++ libgui/plannerwidget.h
@@ -20,7 +20,8 @@
     59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \*************************************************************************/
 
-/* Author: Alexander Perez, Jan Rosell */
+/* Author: Alexander Perez, Jan Rosell, Nestor Garcia Hidalgo */
+
 #if !defined(_PLANNERWIDGET_H)
 #define _PLANNERWIDGET_H
 
