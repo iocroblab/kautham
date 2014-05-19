65ced5eb199f9e8a760b66bd3fb3b6a1717b51b4
Leopold Palomo <leopold.palomo@upc.edu>
2014-05-19 14:32:11 +0200

    - Modification of FindPugiXML.cmake to be more robust. Now accept   standard CMakeParameters of REQUIRED
    - Adapted the sources to use the new module checks.

--- CMakeModules/FindPugiXML.cmake
+++ CMakeModules/FindPugiXML.cmake
@@ -3,31 +3,44 @@
 #
 # The following variabled will be defined:
 #
-#  PUGI_XML_FOUND
-#  PUGI_XML_INCLUDE_DIRS
-#  PUGI_XML_LIBRARY
+# Find the pugixml XML parsing library.
+#
+# Sets the usual variables expected for find_package scripts:
+#
+#  PUGI_XML_FOUND  - true if pugixml was found.
+#  PUGI_XML_INCLUDE_DIRS  - header location
+#  PUGI_XML_LIBRARY  - library to link against
 #
 
 #PUGI_XML
 find_path(PUGI_XML_INCLUDE_DIRS pugixml.hpp
-   PATHS /usr/include
+	  PATHS /usr/include
          /user/local/include)
-if(PUGI_XML_INCLUDE_DIRS)
-   message("Found PUGI_XML_INCLUDE_DIRS  on: ${PUGI_XML_INCLUDE_DIRS}")
-   include_directories(${PUGI_XML_INCLUDE_DIRS})
 
+if(PUGI_XML_INCLUDE_DIRS)
    find_library (PUGI_XML_LIBRARIES
                 NAMES pugixml libpugixml
                 PATHS /usr/lib
                       /usr/local/lib
                       /user/local/lib)
    if(PUGI_XML_LIBRARIES)
-      message("Found PUGI_XML_LIBRARIES on:" ${PUGI_XML_LIBRARIES})
       set(PUGI_XML_FOUND true)
-   else(PUGI_XML_LIBRARIES)
-      message(SEND_ERROR "Not found PUGI_XML_LIBRARIES")
    endif(PUGI_XML_LIBRARIES)
-else(PUGI_XML_INCLUDE_DIRS)
-   message(SEND_ERROR "Not found PUGI_XML_INCLUDE_DIRS")
 endif(PUGI_XML_INCLUDE_DIRS)
+
+# Support the REQUIRED and QUIET arguments, and set PUGIXML_FOUND if found.
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (PugiXML REQUIRED_VARS
+                                  PUGI_XML_LIBRARIES
+                                  PUGI_XML_INCLUDE_DIRS)
+
+if (PUGI_XML_FOUND)
+    set (PUGIXML_LIBRARIES ${PUGIXML_LIBRARY})
+    message (STATUS "Found PugiXML includes on: ${PUGI_XML_INCLUDE_DIRS}")
+    message (STATUS "Found PugiXML libraries on: ${PUGI_XML_LIBRARIES}")
+else (PUGI_XML_FOUND)
+    message (STATUS "No PugiXML found")
+endif(PUGI_XML_FOUND)
+
+mark_as_advanced (PUGI_XML_LIBRARY PUGI_XML_INCLUDE_DIR)
 
