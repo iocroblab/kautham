95cc1e818f7e3706ce64bbfd9eb97eca589e011d
Néstor García Hidalgo <nestorgh91@gmail.com>
2014-05-25 09:44:27 +0200

    Deleted sliders from DOF Widget because a slider which can not be moved by the user is confusing and DOF lower and higher limits are shown in the Problem Tree Widget

--- src/applications/graphical/dofwidget.cpp
+++ src/applications/graphical/dofwidget.cpp
@@ -20,124 +20,124 @@
     59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \*************************************************************************/
 
+
 /* Author: Nestor Garcia Hidalgo */
- 
-#include <QtGui>
+
+
 #include "dofwidget.h"
-#include <QString>
 
 
 namespace Kautham {
+    DOFWidget::DOFWidget(Robot* robot):QWidget() {
+        setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
 
-    DOFWidget::DOFWidget(Robot* rob) {
-        names = QString(rob->getDOFNames().c_str()).split("|");
-
-        QWidget* tmpWid = new QWidget();
-
-        gridLayout = new QGridLayout(tmpWid);
+        QGridLayout *gridLayout = new QGridLayout();
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        setLayout(gridLayout);
 
-        vboxLayout = new QVBoxLayout();
-        vboxLayout->setObjectName(QString::fromUtf8("vboxLayout"));
+        QScrollArea* scrollArea = new QScrollArea();
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        gridLayout->addWidget(scrollArea);
 
-        QLabel* tmpLab;
-        QSlider* tmpSli;
-        QString name;
+        QWidget *scrollAreaWidget = new QWidget();
+        scrollAreaWidget->setObjectName(QString::fromUtf8("scrollAreaWidget"));
+        scrollArea->setWidget(scrollAreaWidget);
+        scrollArea->setWidgetResizable(true);
+
+        QVBoxLayout *vBoxLayout = new QVBoxLayout();
+        vBoxLayout->setObjectName(QString::fromUtf8("vBoxLayout"));
+        vBoxLayout->setContentsMargins(6,6,6,6);
+        vBoxLayout->setSpacing(6);
+        scrollAreaWidget->setLayout(vBoxLayout);
+
+        QStringList names = QString(robot->getDOFNames().c_str()).split("|");
 
         low.resize(names.size());
         high.resize(names.size());
         for (uint i = 0; i < 3; i++) {
             //label value will be defined in milimeters
-            low[i] = rob->getLimits(i)[0];
-            high[i] = rob->getLimits(i)[1];
+            low[i] = robot->getLimits(i)[0];
+            high[i] = robot->getLimits(i)[1];
         }
         for (uint i = 3; i < 6; i++) {
             //label value will be unitary and adimesional
             low[i] = 0.;
             high[i] = 1.;
         }
-        double to_degrees = 180./M_PI;
         for (uint i = 6; i < names.size(); i++) {
-            //label value will be defined in degrees
-            low[i] = *rob->getLink(i-5)->getLimits(true);
-            high[i] = *rob->getLink(i-5)->getLimits(false);
-
-            if (rob->getLink(i-5)->getRotational()) {
-                low[i] *= to_degrees;
-                high[i] *= to_degrees;
-            }
+            //label value will be defined in radians
+            low[i] = *robot->getLink(i-5)->getLimits(true);
+            high[i] = *robot->getLink(i-5)->getLimits(false);
         }
 
+        QString name;
+        QFrame *frame;
+        QVBoxLayout *frameLayout;
+        QLabel *label;
         values.resize(names.size());
         labels.resize(names.size());
-        sliders.resize(names.size());
         for (uint i = 0; i < names.size(); i++){
-            values[i] = rob->getOffMatrix()[i];
+            values[i] = robot->getOffMatrix()[i];
 
-            tmpLab = new QLabel(this);
-            name = names.at(i).toUtf8().constData();
-            tmpLab->setObjectName(name);
-            vboxLayout->addWidget(tmpLab);
-            labels[i] = tmpLab;
+            name = names.at(i);
 
-            tmpSli = new QSlider(this);
-            tmpSli->setObjectName(names.at(i).toUtf8().constData());
-            tmpSli->setOrientation(Qt::Horizontal);
-            tmpSli->setMinimum(0);
-            tmpSli->setMaximum(1000);
-            tmpSli->setSingleStep(10);
-            tmpSli->setValue(500);
-            QObject::connect(tmpSli,SIGNAL(valueChanged(int)),SLOT(sliderChanged(int)));
-            vboxLayout->addWidget(tmpSli);
-            sliders[i] = tmpSli;
+            frame = new QFrame();
+            frame->setObjectName(name+"Frame");
+            frame->setFrameShape(QFrame::StyledPanel);
+            vBoxLayout->addWidget(frame);
+
+            frameLayout = new QVBoxLayout();
+            frameLayout->setObjectName(name+"Layout");
+            frameLayout->setContentsMargins(3,3,3,3);
+            frameLayout->setSpacing(1);
+            frame->setLayout(frameLayout);
+
+            label = new QLabel("<b>"+name+"</b>");
+            label->setObjectName(name+"Name");
+            frameLayout->addWidget(label);
+
+            label = new QLabel();
+            label->setObjectName(name+"Value");
+            if (i < 3) {
+                label->setToolTip("milimeters");
+            } else if (i > 5) {
+                label->setToolTip(robot->getLink(i-6)->getRotational()?"radians":"milimeters");
+            }
+            label->setIndent(9);
+            frameLayout->addWidget(label);
+            labels[i] = label;
         }
 
         setValues(values);
-
-        gridLayout->addLayout(vboxLayout, 0, 1, 1, 1);
-
-        QScrollArea* scrollArea = new QScrollArea();
-        scrollArea->setWidget(tmpWid);
-        scrollArea->setWidgetResizable(true);
-
-        QGridLayout *grid;
-        grid = new QGridLayout(this);
-        grid->addWidget(scrollArea);
     }
 
-    DOFWidget::~DOFWidget(){
-        for (uint i = 0; i < sliders.size(); i++){
-            delete (QSlider*)sliders[i];
+    DOFWidget::~DOFWidget() {
+        for (uint i = 0; i < values.size(); i++) {
             delete (QLabel*)labels[i];
         }
         values.clear();
+        low.clear();
+        high.clear();
     }
 
-    void DOFWidget::sliderChanged(int value){
-        //values only can be changed if the control sliders changed
-        setValues(values);
+
+    void DOFWidget::writeGUI(string text) {
+        emit sendText(text);
     }
 
-    void DOFWidget::writeGUI(string text){
-      emit sendText(text);
-    }
 
-    void DOFWidget::setValues(vector<KthReal> &val){
-        if (val.size() == sliders.size()){
+    void DOFWidget::setValues(vector<KthReal> &val) {
+        if (val.size() == labels.size()) {
             double realval;
-            for(uint i = 0; i < val.size(); i++) {
+            for (uint i = 0; i < val.size(); i++) {
                 values[i] = val.at(i);
                 if (values[i] > 1.0) values[i] = 1.0;
                 if (values[i] < 0.0) values[i] = 0.0;
 
-                ((QSlider*)sliders[i])->setValue((int)(values[i]*1000.0));
-
                 realval = low[i] + values[i]*(high[i]-low[i]);
-                ((QLabel*)labels[i])->setText(QString(names[i] + " = " +
-                                                      QString::number(realval,'f',3)));
+                ((QLabel*)labels[i])->setText(QString::number(realval,'f',3));
             }
         }
     }
-
 }
 
