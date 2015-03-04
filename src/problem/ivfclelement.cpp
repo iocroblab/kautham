a06f87effcc0a8b16d5d39dd79badf3f16a87372
Nestor Garcia <nestorgh91@gmail.com>
2015-03-04 11:12:47 +0100

    Optimized FCL model - not repeated vertexs

--- src/problem/ivfclelement.cpp
+++ src/problem/ivfclelement.cpp
@@ -26,51 +26,79 @@
 #ifdef KAUTHAM_USE_FCL
 
 #include "ivfclelement.h"
-#include <fcl/collision_data.h>
+#include <util/kthutil/kauthamdefs.h>
+#include <map>
+
 #include <fcl/collision.h>
 #include <fcl/distance.h>
+#include <fcl/BVH/BVH_model.h>
 
+#include <Inventor/SoPrimitiveVertex.h>
+#include <Inventor/SbColor.h>
+#include <Inventor/actions/SoCallbackAction.h>
+#include <Inventor/nodes/SoVertexProperty.h>
+#include <Inventor/nodes/SoIndexedFaceSet.h>
 
-struct tri_info {
+struct GeomData {
     vector<fcl::Vec3f> vertices;
     vector<fcl::Triangle> triangles;
+    unsigned int addVertex(const float *point) {
+        fcl::Vec3f vertex(point[0],point[1],point[2]);
+        map<fcl::Vec3f,unsigned int>::const_iterator it = verticesMap.find(vertex);
+        if (it != verticesMap.end()) {
+            return it->second;
+        } else {
+            unsigned int index = vertices.size();
+            vertices.push_back(vertex);
+            verticesMap.insert(pair<fcl::Vec3f,unsigned int>(vertex,index));
+            return index;
+        }
+    }
+private:
+    struct cmpVertex {
+        bool operator()(const fcl::Vec3f& a, const fcl::Vec3f& b) const {
+            return (a[0] < b[0]) ||
+                   (a[1] < b[1] && a[0] == b[0]) ||
+                   (a[2] < b[2] && a[1] == b[1] && a[0] == b[0]);
+        }
+    };
+    map<fcl::Vec3f,unsigned int,cmpVertex> verticesMap;
 };
 
-
-void triang_CB(void *data, SoCallbackAction *action,
-                             const SoPrimitiveVertex *vertex1,
-                             const SoPrimitiveVertex *vertex2,
-                             const SoPrimitiveVertex *vertex3) {
-    tri_info* info = (tri_info*)data;
-
+void triangleCB(void *data, SoCallbackAction *action,
+                const SoPrimitiveVertex *vertex1,
+                const SoPrimitiveVertex *vertex2,
+                const SoPrimitiveVertex *vertex3) {
+    GeomData *geomData = (GeomData*)data;
+    SbVec3f point;
     const SbVec3f vertex[] = {vertex1->getPoint(),
                               vertex2->getPoint(),
                               vertex3->getPoint()};
-    SbVec3f point;
-
     const SbMatrix  matrix = action->getModelMatrix();
+    unsigned int index[3];
     for (int i = 0; i < 3; ++i) {
         matrix.multVecMatrix(vertex[i],point);
 
-        info->vertices.push_back(fcl::Vec3f(point[0],point[1],point[2]));
+        index[i] = geomData->addVertex(point.getValue());
     }
 
-    info->triangles.push_back(fcl::Triangle(info->vertices.size()-3,
-                                            info->vertices.size()-2,
-                                            info->vertices.size()-1));
+    geomData->triangles.push_back(fcl::Triangle(index[0],index[1],index[2]));
 }
 
 
 namespace Kautham {
 
+LCPRNG IVFCLElement::gen = LCPRNG(3141592621,1,0,((unsigned int)time(NULL)&0xfffffffe)+1);
+
 IVFCLElement::IVFCLElement(string visFile, string collFile, KthReal sc, bool useBBOX)
-    : IVElement(visFile,collFile,sc,useBBOX) {
+    : IVElement(visFile,collFile,sc,useBBOX),FCLModel(NULL) {
     if (!makeFCLModel()) throw invalid_argument("FCL model could not be initialized");
 }
 
 
 IVFCLElement::IVFCLElement(SoSeparator *visModel, SoSeparator *collModel, KthReal sc,
-                           bool useBBOX) : IVElement(visModel,collModel,sc,useBBOX) {
+                           bool useBBOX) : IVElement(visModel,collModel,sc,useBBOX),
+    FCLModel(NULL) {
     if (!makeFCLModel()) throw invalid_argument("FCL model could not be initialized");
 }
 
@@ -111,56 +139,57 @@ KthReal IVFCLElement::getDistanceTo(Element* other) {
 
 
 SoSeparator *IVFCLElement::getIvFromFCLModel(bool tran) {
-    float r,g,b,x,y,z;
-    int k;
-    LCPRNG gen(3141592621,1,0,((unsigned int)time(NULL)&0xfffffffe)+1);
-    SbVec3f vertices[3];
-    SoSeparator *root = new SoSeparator;
-    SoSeparator *triangle;
-    SoVertexProperty *vertexProperty;
-    SoFaceSet *faceSet;
-
     fcl::BVHModel<fcl::OBBRSS> *geom;
     geom = (fcl::BVHModel<fcl::OBBRSS>*)FCLModel->collisionGeometry().get();
+    unsigned int nVerts = geom->num_vertices;
+    unsigned int nTris = geom->num_tris;
 
-    for (int i = 0; i < geom->num_tris; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            k = geom->tri_indices[i][j];
-
-            x = geom->vertices[k][0];
-            y = geom->vertices[k][1];
-            z = geom->vertices[k][2];
-
-            vertices[j].setValue(x,y,z);
+    if (nVerts > 2 && nTris > 0) {
+        float vertices[nVerts][3];
+        for (unsigned int i = 0; i < nVerts; ++i) {
+            for (unsigned int j = 0; j < 3; ++j) {
+                vertices[i][j] = geom->vertices[i][j];
+            }
         }
 
-        vertexProperty = new SoVertexProperty;
-        vertexProperty->normalBinding = SoNormalBinding::PER_FACE;
-        vertexProperty->vertex.setValues(0,3,vertices);
-        r = gen.d_rand();
-        g = gen.d_rand();
-        b = gen.d_rand();
-        vertexProperty->orderedRGBA.setValue(SbColor(r,g,b).getPackedValue());
+        uint32_t colors[nTris];
+        int32_t coordIndices[nTris*4];
+        int32_t materialIndices[nTris*4];
+        for (unsigned int i = 0; i < nTris; ++i) {
+            for (unsigned int j = 0; j < 3; ++j) {
+                coordIndices[4*i+j] = geom->tri_indices[i][j];
+                materialIndices[4*i+j] = i;
+            }
+            coordIndices[4*i+3] = SO_END_FACE_INDEX;
+            materialIndices[4*i+3] = SO_END_FACE_INDEX;
+            colors[i] = SbColor(gen.d_rand(),gen.d_rand(),gen.d_rand()).getPackedValue();
+        }
 
-        faceSet = new SoFaceSet;
-        faceSet->numVertices.setValue(3);
-        faceSet->vertexProperty.setValue(vertexProperty);
+        SoVertexProperty *vertexProperty(new SoVertexProperty);
+        vertexProperty->vertex.setValues(0,nVerts,vertices);
+        vertexProperty->orderedRGBA.setValues(0,nTris,colors);
+        vertexProperty->materialBinding.setValue(SoVertexProperty::PER_VERTEX_INDEXED);
 
-        triangle = new SoSeparator();
-        triangle->addChild(faceSet);
+        SoIndexedFaceSet *indexedFaceSet(new SoIndexedFaceSet());
+        indexedFaceSet->coordIndex.setValues(0,nTris*4,coordIndices);
+        indexedFaceSet->materialIndex.setValues(0,nTris*4,materialIndices);
+        indexedFaceSet->vertexProperty.setValue(vertexProperty);
 
-        root->addChild(triangle);
-    }
+        SoSeparator *root(new SoSeparator);
+        root->addChild(indexedFaceSet);
 
-    if (tran) {
-        SoSeparator* temp = new SoSeparator;
-        temp->addChild(getTrans());
-        temp->addChild(getRot());
-        temp->addChild(root);
+        if (tran) {
+            SoSeparator* temp = new SoSeparator;
+            temp->addChild(getTrans());
+            temp->addChild(getRot());
+            temp->addChild(root);
 
-        return temp;
+            return temp;
+        } else {
+            return root;
+        }
     } else {
-        return root;
+        return NULL;
     }
 }
 
@@ -190,14 +219,14 @@ bool IVFCLElement::makeFCLModel() {
     try {
         if (collision_ivModel()) {
             SoCallbackAction triAction;
-            tri_info info;
+            GeomData geomData;
             triAction.addTriangleCallback(SoShape::getClassTypeId(),
-                                          triang_CB, (void*)&info);
+                                          triangleCB,(void*)&geomData);
             triAction.apply(collision_ivModel());
 
             fcl::BVHModel<fcl::OBBRSS> *geom = new fcl::BVHModel<fcl::OBBRSS>;
             geom->beginModel();
-            geom->addSubModel(info.vertices, info.triangles);
+            geom->addSubModel(geomData.vertices,geomData.triangles);
             geom->endModel();
 
             const boost::shared_ptr<fcl::CollisionGeometry> cgeom(geom);
