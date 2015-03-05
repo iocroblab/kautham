87328ee92ebaf140568e094bd65939eb534fd7fd
Néstor García Hidalgo <nestorgh91@gmail.com>
2015-03-06 00:22:45 +0100

    Added Path Length Weight to RRT* and TRRT

--- src/planner/omplg/omplMyOptimizationObjective.h
+++ src/planner/omplg/omplMyOptimizationObjective.h
@@ -44,17 +44,17 @@ namespace Kautham {
 
   class myMWOptimizationObjective : public ob::MechanicalWorkOptimizationObjective {
   private:
-      std::vector< std::vector<double> > controlpoints;
-      std::vector< std::pair<double,double> > costParams;
+      std::vector<std::vector<double> > controlpoints;
+      std::vector<std::pair<double,double> > costParams;
       omplPlanner *pl;
   public:
       myMWOptimizationObjective(const ob::SpaceInformationPtr &si, omplPlanner *p,
                                 double pathLengthWeight = 0.00001);
-      void setControlPoints(std::vector< std::vector<double> > *cp);
-      void setCostParams(std::vector<std::pair<double, double> > *cp);
+      void setControlPoints(std::vector<std::vector<double> > *cp);
+      void setCostParams(std::vector<std::pair<double,double> > *cp);
       void setPathLengthWeight(double weight) {pathLengthWeight_ = weight;}
       bool isSymmetric() {return false;}
-      virtual ob::Cost 	stateCost (const ob::State *s) const;
+      virtual ob::Cost stateCost(const ob::State *s) const;
   };
 
   class myICOptimizationObjective : public ob::MechanicalWorkOptimizationObjective {
@@ -62,20 +62,23 @@ namespace Kautham {
       std::vector< std::vector<double> > controlpoints;
       std::vector< std::pair<double,double> > costParams;
       omplPlanner *pl;
+      double pathLengthWeight_;
       bool interpolateMotionCost_;
 
       ob::Cost trapezoid(ob::Cost c1, ob::Cost c2, double dist) const {
-          return ob::Cost(0.5 * dist * (c1.v + c2.v));
+          return ob::Cost(0.5*dist*(c1.v+c2.v));
       }
   public:
       myICOptimizationObjective(const ob::SpaceInformationPtr &si, omplPlanner *p,
+                                double pathLengthWeight = 0.00001,
                                 bool enableMotionCostInterpolation = false);
       void setControlPoints(std::vector< std::vector<double> > *cp);
       void setCostParams(std::vector<std::pair<double, double> > *cp);
+      void setPathLengthWeight(double weight) {pathLengthWeight_ = weight;}
       bool isSymmetric() {return ob::OptimizationObjective::isSymmetric();}
       bool isMotionCostInterpolationEnabled() const {return interpolateMotionCost_;}
-      virtual ob::Cost 	stateCost (const ob::State *s) const;
-      virtual ob::Cost 	motionCost (const ob::State *s1, const ob::State *s2) const;
+      virtual ob::Cost stateCost(const ob::State *s) const;
+      virtual ob::Cost motionCost(const ob::State *s1, const ob::State *s2) const;
     };
   }
   /** @}   end of Doxygen module "Planner */
