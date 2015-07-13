8a43ff425eb282cdc5d8a3846b96dacf2de161cf
Néstor García Hidalgo <nestorgh91@gmail.com>
2015-07-13 10:27:30 +0200

    Updated demos

---
+++ demos/OMPL_demos/SMC15/crides.m
@@ -0,0 +1,78 @@
+roscore
+
+
+cd ~/catkin_ws/devel_isolated/kautham2/lib/kautham2
+./Kautham_node
+
+
+cd ~/catkin_ws/build_isolated/goal_generator/
+./goal_generator
+
+
+cd ~/catkin_ws/build_isolated/query_generator/
+./query_generator
+
+
+cd ~/Documents/kautham/demos/OMPL_demos/SMC15
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/water_with_box_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/water_with_box_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_box_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_box_PMD 10 water_with_box_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/water_with_cylinder_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/water_with_cylinder_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_cylinder_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_cylinder_PMD 10 water_with_cylinder_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/water_with_free_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/water_with_free_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_free_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_free_PMD 10 water_with_free_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/water_with_no_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/water_with_no_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_no_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_no_PMD 10 water_with_no_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/water_with_water_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/water_with_water_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_water_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 water_with_water_PMD 10 water_with_water_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/cylinder_with_box_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/cylinder_with_box_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_box_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_box_PMD 10 cylinder_with_box_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/cylinder_with_cylinder_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/cylinder_with_cylinder_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_cylinder_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_cylinder_PMD 10 cylinder_with_cylinder_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/cylinder_with_free_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/cylinder_with_free_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_free_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_free_PMD 10 cylinder_with_free_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/cylinder_with_no_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/cylinder_with_no_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_no_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_no_PMD 10 cylinder_with_no_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/cylinder_with_water_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/cylinder_with_water_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_water_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 cylinder_with_water_PMD 10 cylinder_with_water_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/box_with_box_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/box_with_box_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_box_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_box_PMD 10 box_with_box_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/box_with_cylinder_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/box_with_cylinder_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_cylinder_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_cylinder_PMD 10 box_with_cylinder_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/box_with_free_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/box_with_free_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_free_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_free_PMD 10 box_with_free_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/box_with_no_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/box_with_no_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_no_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_no_PMD 10 box_with_no_PMD.txt
+
+./../../../../bihapca/build/SMC15/main/benchmarkingGenerator ~/Documents/box_with_water_PMD_goals.xml ~/Documents/kautham/demos/OMPL_demos/SMC15/box_with_water_PMD.xml ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_water_PMD
+./../../../build/src/applications/console/KauthamConsole -b benchmarking.xml
+./../../../../bihapca/build/SMC15/main/benchmarkingParser ~/Documents/kautham/demos/OMPL_demos/SMC15 box_with_water_PMD 10 box_with_water_PMD.txt
+
+
