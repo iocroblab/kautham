cef03402a2f069aea9a4c50f46b47dbabe639c24
Ali Debian <ali.akbari101@gmail.com>
2016-04-08 17:42:01 +0200

    Merged cline node

--- src/applications/ros/manipulation_client.cpp
+++ src/applications/ros/manipulation_client.cpp
@@ -45,6 +45,8 @@
 #include"kautham2/GetWorldState.h"
 #include"kautham2/SetWorldState.h"
 #include"kautham2/SolveManipQuery.h"
+#include"kautham2/CheckCollision.h"
+
 using namespace std;
 using namespace Kautham;
 
@@ -52,11 +54,100 @@ using namespace Kautham;
 //ros::Publisher chatter_pub;
 kauthamshell* kmanip;
 
+//Compute distance to goal
 double dist(std::vector<double>laststate, std::vector<double>goal)
 {
     double distGoal;
     distGoal = sqrt((pow((laststate[0] - goal[0]), 2) + (pow((laststate[1] - goal[1]), 2))));
     return distGoal;
+}
+
+//Creat a potential goal for push/pull
+std::vector<double> potGoal(std::vector<double>objPos,std::vector<double>robPos, std::string action, std::string know)
+{
+    std::vector<double>goalPos;
+    goalPos.resize(2);
+
+    std::vector<double>goalPos1;
+    goalPos1.resize(2);
+
+    std::vector<double>goalPos2;
+    goalPos2.resize(2);
+
+    if(know == "x")
+    {
+        double posx = objPos[0] + 20; // 20 will be input for all.
+        double negx = objPos[0] - 20;
+
+        goalPos1[0]=posx;
+        goalPos1[1]=objPos[1];
+
+        goalPos2[0]=negx;
+        goalPos2[1]=objPos[1];
+
+        double d1 = dist(goalPos1, robPos);
+        double d2 = dist(goalPos2, robPos);
+
+        if(d1<d2 && action=="pull")
+        {
+            goalPos=goalPos1;
+        }
+
+        if(d1>d2 && action=="pull")
+        {
+            goalPos=goalPos2;
+        }
+
+        if(d1<d2 && action=="push")
+        {
+            goalPos=goalPos2;
+        }
+
+        if(d1>d2 && action=="push")
+        {
+            goalPos=goalPos1;
+        }
+    }
+
+
+    if(know == "y")
+    {
+        double posy = objPos[0] + 20; // 20 will be input for all.
+        double negy = objPos[0] - 20;
+
+        goalPos1[0]=objPos[0];
+        goalPos1[1]=posy;
+
+        goalPos2[0]=objPos[0];
+        goalPos2[1]=negy;
+
+        double d1 = dist(goalPos1, robPos);
+        double d2 = dist(goalPos2, robPos);
+
+        if(d1<d2 && action=="pull")
+        {
+            goalPos=goalPos1;
+        }
+
+        if(d1>d2 && action=="pull")
+        {
+            goalPos=goalPos2;
+        }
+
+        if(d1<d2 && action=="push")
+        {
+            goalPos=goalPos2;
+        }
+
+        if(d1>d2 && action=="push")
+        {
+            goalPos=goalPos1;
+        }
+    }
+
+
+    return goalPos;
+
 }
 
 void getWorldState()
@@ -98,16 +189,16 @@ void setBodyState()
     ros::service::waitForService("manipulation_node/SetBodyState");
     ros::ServiceClient SetBodyState_client = n.serviceClient<kautham2::SetBodyState>("manipulation_node/SetBodyState");
     kautham2::SetBodyState SetBodyState_srv;
-    SetBodyState_srv.request.taretBody=0;
+    SetBodyState_srv.request.taretBody=4;
     //dummy value to checek the functionality of the service
     SetBodyState_srv.request.Pose.resize(7);
-    SetBodyState_srv.request.Pose[0]=20;
-    SetBodyState_srv.request.Pose[1]=20;
-    SetBodyState_srv.request.Pose[2]=20;
-    SetBodyState_srv.request.Pose[3]=1;
+    SetBodyState_srv.request.Pose[0]=-105;
+    SetBodyState_srv.request.Pose[1]=145;
+    SetBodyState_srv.request.Pose[2]=15;
+    SetBodyState_srv.request.Pose[3]=0.7;
     SetBodyState_srv.request.Pose[4]=0;
     SetBodyState_srv.request.Pose[5]=0;
-    SetBodyState_srv.request.Pose[6]=0;
+    SetBodyState_srv.request.Pose[6]=0.7;
 
     SetBodyState_client.call(SetBodyState_srv);
     ROS_DEBUG("Set Body state service performed sucessfully !");
@@ -119,8 +210,11 @@ void getBodyState()
     ros::service::waitForService("manipulation_node/GetBodyState");
     ros::ServiceClient getBodyState_client = n.serviceClient<kautham2::GetBodyState>("manipulation_node/GetBodyState");
     kautham2::GetBodyState getBodyState_srv;
-    getBodyState_srv.request.taretBody=0;
+    getBodyState_srv.request.taretBody=4;
     getBodyState_client.call(getBodyState_srv);
+
+    std::cout<<"Body position is : "<<getBodyState_srv.response.targetBodyPose[0]<<" "<<getBodyState_srv.response.targetBodyPose[1]<<std::endl;
+
     ROS_DEBUG("Get Body state service performed sucessfully !");
 }
 
@@ -130,8 +224,8 @@ std::vector<double>contConversion(std::vector<double> pose)
     double minX, maxX, minY, maxY;
     minX = -285.0;
     maxX = 285.0;
-    minY = -185.0;
-    maxY = 185.0;
+    minY = -153.0;
+    maxY = 153.0;
     std::vector<double>cont;
     cont.resize(2);
     cont[0] = (pose[0] - minX)/(maxX - minX);
@@ -148,13 +242,17 @@ void setManipQuery()
     ros::ServiceClient open_problem_client = node.serviceClient<kautham2::OpenManipProblem>("manipulation_node/OpenManipProblem");
     kautham2::OpenManipProblem open_problem_srv;
     std::string model = "/home/muhayyuddin/catkin_ws/src/kautham/demos/models";
-
     open_problem_srv.request.problem = "/home/users/aliakbar.akbari/catkin_ws/src/kautham/demos/OMPL_demos/KauthamOpenDE/ETFA2016.xml";
     //open_problem_srv.request.problem = "/home/muhayyuddin/catkin_ws/src/kautham/demos/OMPL_demos/KauthamOpenDE/ETFA2016.xml";
     open_problem_srv.request.dir.resize(1);
     open_problem_srv.request.dir[0] = model;
     open_problem_client.call(open_problem_srv);
     // activate the setquery service
+
+    //Set the new object pose
+//    getBodyState();
+//    setBodyState();
+//    getBodyState();
 
     ros::service::waitForService("manipulation_node/SolveManipQuery");
     ros::ServiceClient solve_manip_query_client = node.serviceClient<kautham2::SolveManipQuery>("manipulation_node/SolveManipQuery");
@@ -170,12 +268,12 @@ void setManipQuery()
     std::vector<double>goalC;
 
     init.resize(2);
-    init[0]=228;
-    init[1]=121;
+    init[0]=82;
+    init[1]=-25;
 
     goal.resize(2);
-    goal[0]=78;
-    goal[1]=-35;
+    goal[0]=82;
+    goal[1]=67;
 
     initC = contConversion(init);
     goalC = contConversion(goal);
@@ -185,21 +283,21 @@ void setManipQuery()
 
 
     // Set the initial and goal vectors (kautham controls)
-    solve_manip_query_srv.request.init[0] = 0.481;
-    solve_manip_query_srv.request.init[1] = 0.548;
+    solve_manip_query_srv.request.init[0] = initC[0];
+    solve_manip_query_srv.request.init[1] = initC[1];
     //solve_manip_query_srv.request.init[2] = initC[2];
-    solve_manip_query_srv.request.goal[0] = 0.11;
-    solve_manip_query_srv.request.goal[1] = 0.176;
+    solve_manip_query_srv.request.goal[0] = goalC[0];
+    solve_manip_query_srv.request.goal[1] = goalC[1];
     //solve_manip_query_srv.request.goal[2] = goalC[2];
 
     solve_manip_query_srv.request.actionType="move";
 
-    // Just for push and pull.
-    solve_manip_query_srv.request.force.resize(3);
-    solve_manip_query_srv.request.force[0] = 0.0;
-    solve_manip_query_srv.request.force[1] = -5.0;
-    solve_manip_query_srv.request.force[2] = 0.0;
-    solve_manip_query_srv.request.targetBody=5;
+//    // Just for push and pull.
+//    solve_manip_query_srv.request.force.resize(3);
+//    solve_manip_query_srv.request.force[0] = 0.0;
+//    solve_manip_query_srv.request.force[1] = -5.0;
+//    solve_manip_query_srv.request.force[2] = 0.0;
+//    solve_manip_query_srv.request.targetBody=5;
 
 
     //call kautham service to set the query
@@ -224,7 +322,15 @@ void setManipQuery()
         std::cout<<"Motion query has not been solved"<<std::endl;
     }
 
+
     ROS_INFO("Manipulation Query performed sucessfully !");
+
+
+    //    //Set check collision and return the index of object collided if any.
+    //    ros::service::waitForService("kautham_node/CheckCollision");
+    //    ros::ServiceClient check_collision_client = node.serviceClient<kautham2::CheckCollision>("kautham_node/CheckCollision");
+    //    kautham2::CheckCollision check_collision_srv;
+
 
 }
 
@@ -236,6 +342,24 @@ int main (int argc, char **argv)
     ROS_INFO("Starting Manipulation_Client");
 
     setManipQuery();
+
+   std::vector<double>objPos;
+   objPos.resize(2);
+   objPos[0]=5;
+   objPos[1]=2;
+
+   std::vector<double>robPos;
+   robPos.resize(2);
+   robPos[0]=4;
+   robPos[1]=2;
+
+   std::string action;
+   action = "pull";
+
+   std::string know;
+   know = "x";
+
+   potGoal(objPos, robPos, action, know);
 
     //    getBodyState();
     //    setBodyState();
