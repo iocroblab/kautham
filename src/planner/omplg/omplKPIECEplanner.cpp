6f9bdb78acc926a4e5e5832070a83e677839e43a
Jan Rosell <jan.rosell@upc.edu>
2017-03-13 17:35:23 +0100

    Inherited og::KPIECE to create myKPIECE. Reimplemented solve function to grow several steps per motion + limiting growing when steered by goal. Still not solving simple Table-rooms problem

--- src/planner/omplg/omplKPIECEplanner.cpp
+++ src/planner/omplg/omplKPIECEplanner.cpp
@@ -32,10 +32,204 @@
 #include <kautham/planner/omplg/omplKPIECEplanner.h>
 #include <kautham/planner/omplg/omplValidityChecker.h>
 
-
+#include <ompl/geometric/planners/kpiece/KPIECE1.h>
+#include <ompl/base/goals/GoalSampleableRegion.h>
+#include <ompl/tools/config/SelfConfig.h>
+#include <ompl/geometric/planners/kpiece/Discretization.h>
+#include <limits>
+#include <cassert>
 
 namespace Kautham {
   namespace omplplanner{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    class myKPIECE1:public og::KPIECE1
+    {
+
+        public:
+        myKPIECE1( ob::SpaceInformationPtr &si) : KPIECE1(si)
+        {
+
+        }
+        ob::PlannerStatus solve(const ob::PlannerTerminationCondition &ptc)
+        {
+            checkValidity();
+            ob::Goal *goal = pdef_->getGoal().get();
+            ob::GoalSampleableRegion *goal_s = dynamic_cast<ob::GoalSampleableRegion *>(goal);
+
+            og::Discretization<Motion>::Coord xcoord;
+
+            while (const ob::State *st = pis_.nextStart())
+            {
+                auto *motion = new Motion(si_);
+                si_->copyState(motion->state, st);
+                projectionEvaluator_->computeCoordinates(motion->state, xcoord);
+                disc_.addMotion(motion, xcoord, 1.0);
+            }
+
+            if (disc_.getMotionCount() == 0)
+            {
+                OMPL_ERROR("%s: There are no valid initial states!", getName().c_str());
+                return ob::PlannerStatus::INVALID_START;
+            }
+
+            if (!sampler_)
+                sampler_ = si_->allocStateSampler();
+
+            OMPL_INFORM("%s: Starting planning with %u states already in datastructure", getName().c_str(),
+                        disc_.getMotionCount());
+
+            Motion *solution = nullptr;
+            Motion *approxsol = nullptr;
+            double approxdif = std::numeric_limits<double>::infinity();
+            ob::State *xstate = si_->allocState();
+
+            int maxsteps = 10;//do at most maxsteps in a given direction
+
+            while (ptc == false)
+            {
+                disc_.countIteration();
+
+                /* Decide on a state to expand from */
+                Motion *existing = nullptr;
+                og::Discretization<Motion>::Cell *ecell = nullptr;
+                disc_.selectMotion(existing, ecell);
+                assert(existing);
+
+                /* sample random state (with goal biasing) */
+                if (goal_s && rng_.uniform01() < goalBias_ && goal_s->canSample())
+                {
+                    goal_s->sampleGoal(xstate);
+                    /* find state to add */
+                    double d = si_->distance(existing->state, xstate);
+                    if (d > maxsteps*maxDistance_)
+                    {
+                        si_->getStateSpace()->interpolate(existing->state, xstate, maxsteps*maxDistance_ / d, xstate);
+                    }
+                }
+                else
+                    sampler_->sampleUniformNear(xstate, existing->state, maxsteps*maxDistance_);
+
+                //std::pair<ob::State *, double> fail(xstate, 0.0);
+                //bool keep = si_->checkMotion(existing->state, xstate, fail);
+                //if (!keep && fail.second > minValidPathFraction_)
+                //    keep = true;
+                //if (keep)
+
+
+//                if (si_->checkMotion(existing->state, xstate))
+//                {
+//                    /* create a motion */
+//                    auto *motion = new Motion(si_);
+//                    si_->copyState(motion->state, xstate);
+//                    motion->parent = existing;
+
+//                    double dist = 0.0;
+//                    bool solv = goal->isSatisfied(motion->state, &dist);
+//                    projectionEvaluator_->computeCoordinates(motion->state, xcoord);
+//                    disc_.addMotion(motion, xcoord, dist);  // this will also update the discretization heaps as needed, so no
+//                                                                 // call to updateCell() is needed
+
+//                    if (solv)
+//                    {
+//                        approxdif = dist;
+//                        solution = motion;
+//                        break;
+//                    }
+//                    if (dist < approxdif)
+//                    {
+//                        approxdif = dist;
+//                        approxsol = motion;
+//                    }
+
+//                }
+
+                Motion *m1=existing;
+                ob::State *s1 = existing->state;
+                ob::State *s2 = si_->allocState();;
+                for(int i=1; i< (1+rng_.uniform01()*maxsteps); i++)
+                {
+                    si_->getStateSpace()->interpolate(existing->state, xstate, i*0.25, s2);
+
+                    if (si_->checkMotion(s1, s2))
+                    {
+                        /* create the motion */
+                        auto *motion = new Motion(si_);
+                        si_->copyState(motion->state, s2);
+                        motion->parent = m1;
+
+                        double dist = 0.0;
+                        bool solv = goal->isSatisfied(motion->state, &dist);
+                        projectionEvaluator_->computeCoordinates(motion->state, xcoord);
+                        disc_.addMotion(motion, xcoord, dist);  // this will also update the discretization heaps as needed, so no
+                                                                 // call to updateCell() is needed
+
+                        if (solv)
+                        {
+                            approxdif = dist;
+                            solution = motion;
+                            break;
+                        }
+                        if (dist < approxdif)
+                        {
+                            approxdif = dist;
+                            approxsol = motion;
+                        }
+
+                        //prepare next step
+                        s1=s2;
+                        m1=motion;
+                    }
+                    else
+                    {
+                        ecell->data->score *= failedExpansionScoreFactor_;
+                        break;
+                    }
+                }
+
+
+                 disc_.updateCell(ecell);
+            }
+
+            bool solved = false;
+            bool approximate = false;
+            if (solution == nullptr)
+            {
+                solution = approxsol;
+                approximate = true;
+            }
+
+            if (solution != nullptr)
+            {
+                lastGoalMotion_ = solution;
+
+                /* construct the solution path */
+                std::vector<Motion *> mpath;
+                while (solution != nullptr)
+                {
+                    mpath.push_back(solution);
+                    solution = solution->parent;
+                }
+
+                /* set the solution path */
+                auto path(std::make_shared< og::PathGeometric>(si_));
+                for (int i = mpath.size() - 1; i >= 0; --i)
+                    path->append(mpath[i]->state);
+                pdef_->addSolutionPath(path, approximate, approxdif, getName());
+                solved = true;
+            }
+
+            si_->freeState(xstate);
+
+            OMPL_INFORM("%s: Created %u states in %u cells (%u internal + %u external)", getName().c_str(),
+                        disc_.getMotionCount(), disc_.getCellCount(), disc_.getGrid().countInternal(),
+                        disc_.getGrid().countExternal());
+
+            return ob::PlannerStatus(solved, approximate);
+      }
+    };
+
+
+
 
     //! Constructor
     omplKPIECEPlanner::omplKPIECEPlanner(SPACETYPE stype, Sample *init, Sample *goal, SampleSet *samples, WorkSpace *ws, og::SimpleSetup *ssptr):
@@ -45,7 +239,7 @@ namespace Kautham {
         _idName = "omplKPIECE";
 
         //create planner
-        ob::PlannerPtr planner(new og::KPIECE1(si));
+        ob::PlannerPtr planner(new myKPIECE1(si));
         //set planner parameters: range and goalbias
         _Range=0.05;
         _GoalBias=(planner->as<og::KPIECE1>())->getGoalBias();
